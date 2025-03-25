37ec7166305ebc9a958198b364fdde4305cfd547
Kirill Popov <kirill.s.popov@gmail.com>
2025-03-25 23:26:50 +0300

    Initial version of extractor

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# huntmap-extractor
+# HuntMap geo info extractor
+
+## Description
+The tool is extracting geo data on hunting areas from https://huntmap.ru
+
+Source data for the extractor is to be fetched from the site. For different areas (public, private, forbidden) have different data format so the type of area must be passed as argument. For now only public and private areas supported by converter.
+**WARNING! Forbidden areas are not excluded from public and private areas. These forbidden areas must be manually checked.**
+
+## Usage:
+First install python requirements from `requirements.txt`. `virtualenv` may be used
+Get areas data you're interested in and save it to say `map.js` . The exact URL may be discovered from browser inspector, links to data all start with `https://maps.kosmosnimki.ru/TileSender.ashx` and are usually relatively large.
+Then run the converter with appropriate are type:
+```
+python3 huntmap_extract.py <public|private> < map.js > map.geojson
+```
+The resulting file is GeoJSON which may be vizualized e.g. at https://geojson.tools/ , used in some other viewer or converted to KML.
+
+## Found area links
+- Saint-Petersburg and Leningrad region
+  - Public hunting areas: https://maps.kosmosnimki.ru/TileSender.ashx?WrapStyle=None&ModeKey=tile&ftc=osm&r=j&LayerName=4F7341B1732B4276BFEE27CC553B81BF&z=1&x=1&y=0&v=207&srs=3857&sw=1
+  - Private hunting areas: https://maps.kosmosnimki.ru/TileSender.ashx?WrapStyle=None&ModeKey=tile&ftc=osm&r=j&LayerName=DC6E51836E8A4D8CA12F6098B7DC7FEA&z=1&x=1&y=0&v=990&srs=3857&sw=1
+
+Tested on https://huntmap.ru/karta-oxotnichix-ugodij-leningradskoj-oblasti
---
+++ huntmap_extract.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+
+import sys
+import json
+
+import geojson
+import pyproj
+
+transformer = pyproj.Transformer.from_crs("EPSG:3857", "EPSG:4326")
+
+def coord_convert_and_fix(leaflet_coordinates):
+    # leaflet coordinates are `lat, lon` whereas GeoJSON and KML are `lon, lat`. Source data is for leaflet
+    # https://macwright.com/lonlat/
+    transformed = transformer.transform(*leaflet_coordinates)
+    # need to first thansform and then swap as order matters when converting
+    return [transformed[1], transformed[0]]
+
+def read_place_private(place):
+    # private areas and common areas have different info formats. forbidden areas have different format too
+    # unpack element to readable variable names
+    plc_index, plc_region, plc_org, plc_area, plc_central_addr, plc_phone, plc_email, plc_person, plc_local_addr, plc_geo = place
+    props = {
+        'index': plc_index,
+        'region': plc_region,
+        'org': plc_org,
+        'area': plc_area,
+        'central_address': plc_central_addr,
+        'phone': plc_phone,
+        'email': plc_email,
+        'person': plc_person,
+        'local_address': plc_local_addr
+        }
+    return plc_geo, props
+
+def read_place_public(place):
+    # private areas and common areas have different info formats. forbidden areas have different format too
+    # unpack element to readable variable names
+    plc_index, plc_region, plc_area, plc_local_addr, plc_phone, plc_href, plc_org, plc_geo = place
+    props = {
+        'index': plc_index,
+        'region': plc_region,
+        'org': plc_org,
+        'area': plc_area,
+        'phone': plc_phone,
+        'local_address': plc_local_addr,
+        'href': plc_href
+        }
+    return plc_geo, props
+
+if __name__ == '__main__':
+
+    if len(sys.argv) < 2:
+        raise Exception(f'Usage: {sys.argv[0]} <public|private> < map.js > map.geojson')
+
+    data = json.loads(
+            sys.stdin.read().lstrip('gmxAPI._vectorTileReceiver(').rstrip(')')
+            )
+
+    features = []
+#pgn = geojson.Polygon([[ transformer.transform(*dot)[-1::-1] for dot in data['values'][0][9]['coordinates'][0] ]])
+#features = []
+#features.append(geojson.Feature(geometry=pgn, properties={}))
+#feature_collection = geojson.FeatureCollection(features)
+#with open('myfile.geojson', 'w') as f:
+#   json.dump(feature_collection, f, indent=4)
+    for place in data['values']:
+        if sys.argv[1] == 'private':
+            plc_geo, props = read_place_private(place)
+        elif sys.argv[1] == 'public':
+            plc_geo, props = read_place_public(place)
+        else:
+            raise Exception(f'Usage: {sys.argv[0]} <public|private> < map.js > map.geojson')
+        if plc_geo['type'] != 'POLYGON':
+            # only support polygons
+            continue
+
+        polygon_paths = [] # array of 1st outer polygon contour and later for "holes" in polygon
+        for poly_path in plc_geo['coordinates']:
+            polygon_paths.append(
+                [coord_convert_and_fix(coord) for coord in poly_path]
+                )
+            
+        polygon = geojson.Polygon(polygon_paths)
+        features.append(geojson.Feature(geometry=polygon, properties=props))
+
+    feature_collection = geojson.FeatureCollection(features)
+    sys.stdout.write(
+        json.dumps(feature_collection, indent=2)
+        )
