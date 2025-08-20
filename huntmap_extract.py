f67cdc24c9446c3992d5e2641ee06cded75b4ed0
Kirill Popov <kirill.s.popov@gmail.com>
2025-08-20 16:25:00 +0300

    Added info logging

--- huntmap_extract.py
+++ huntmap_extract.py
@@ -2,6 +2,7 @@
 
 import sys
 import json
+import logging
 
 import geojson
 import pyproj
@@ -30,6 +31,7 @@ def read_place_private(place):
         'person': plc_person,
         'local_address': plc_local_addr
         }
+    logging.info(f'Processed {plc_area} as private place')
     return plc_geo, props
 
 def read_place_public(place):
@@ -45,9 +47,11 @@ def read_place_public(place):
         'local_address': plc_local_addr,
         'href': plc_href
         }
+    logging.info(f'Processed {plc_area} as public place')
     return plc_geo, props
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
 
     if len(sys.argv) < 2:
         raise Exception(f'Usage: {sys.argv[0]} <public|private> < map.js > map.geojson')
@@ -55,6 +59,7 @@ if __name__ == '__main__':
     data = json.loads(
             sys.stdin.read().lstrip('gmxAPI._vectorTileReceiver(').rstrip(')')
             )
+    logging.info(f'Loaded {len(data["values"])} from input')
 
     features = []
 #pgn = geojson.Polygon([[ transformer.transform(*dot)[-1::-1] for dot in data['values'][0][9]['coordinates'][0] ]])
@@ -64,6 +69,7 @@ if __name__ == '__main__':
 #with open('myfile.geojson', 'w') as f:
 #   json.dump(feature_collection, f, indent=4)
     for place in data['values']:
+        logging.info(f'Processing place with index {place[0]}')
         if sys.argv[1] == 'private':
             plc_geo, props = read_place_private(place)
         elif sys.argv[1] == 'public':
@@ -74,6 +80,7 @@ if __name__ == '__main__':
             # only support polygons
             continue
 
+        logging.info(f'Building polygon for place index {props["index"]}')
         polygon_paths = [] # array of 1st outer polygon contour and later for "holes" in polygon
         for poly_path in plc_geo['coordinates']:
             polygon_paths.append(
@@ -83,6 +90,7 @@ if __name__ == '__main__':
         polygon = geojson.Polygon(polygon_paths)
         features.append(geojson.Feature(geometry=polygon, properties=props))
 
+    logging.info(f'Processed all places, forming FeatureCollection')
     feature_collection = geojson.FeatureCollection(features)
     sys.stdout.write(
         json.dumps(feature_collection, indent=2)
